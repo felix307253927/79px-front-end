a2b4c2a1097314e24655fd34d133ede4ebfa6105
eternityspring@163.com <1526qaws>
2018-01-18 22:12:05 +0800

    链接文字修改

--- src/components/header.vue
+++ src/components/header.vue
@@ -4,9 +4,9 @@
       <div class="container">
         <router-link to="/" class="brand-logo">{79px}</router-link>
         <ul id="nav-mobile" class="right hide-on-med-and-down">
-          <li><a href="sass.html">Sass</a></li>
-          <li><a href="badges.html">Components</a></li>
-          <li><a href="collapsible.html">JavaScript</a></li>
+          <li><a href="sass.html">登录</a></li>
+          <li><a href="badges.html">注册</a></li>
+          <li><a href="collapsible.html">注销</a></li>
         </ul>
       </div>
     </div>
