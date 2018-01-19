3a25f718d02f22e8b08a1f073e348d956e7ab0b8
eternityspring <eternityspring@163.com>
2018-01-19 23:26:23 +0800

    首页调整 导航添加 添加探索、推荐页，添加博客页

--- src/components/header.vue
+++ src/components/header.vue
@@ -10,9 +10,9 @@
           <li><router-link to="/blog">博客</router-link></li>
         </ul>
         <ul id="nav-mobile" class="right hide-on-med-and-down">
-          <li><a href="sass.html">登录</a></li>
-          <li><a href="badges.html">注册</a></li>
-          <li><a href="collapsible.html">注销</a></li>
+          <li><a href="">登录</a></li>
+          <li><a href="">注册</a></li>
+          <li><a href="">注销</a></li>
         </ul>
       </div>
     </div>
